d738febef6163b3d65e4fc28faa31b95aee7d091
鸽子 <1803902484@qq.com>
2023-03-17 17:17:37 +0800

    1

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-
-createApp(App).use(store).use(router).mount('#app')
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import Vant from "vant";
+import axios from "axios";
+import "vant/lib/index.css";
+const app = createApp(App);
+app.config.globalProperties.axios = axios;
+app.use(store);// TODO:vuex配置
+app.use(Vant);// TODO:全局引入vant
+app.use(router);
+app.mount("#app");
